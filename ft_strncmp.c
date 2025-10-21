7242b22eb7d0f1ecd7a6d58f3d26c097dcfda0d2
unknown <victormesini007@gmail.com>
2025-10-21 14:38:37 +0100

    corrections strncmp and strlcpy

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,23 +17,22 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	int	i;
 
 	i = 0;
-	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 		i++;
 	}
-	return (s1[i - s2[i]]);
+	return (s1[i]- s2[i]);
 }
 
-/* int main()
-{
-	char	*s1;
-	char	*s2;
+// int main()
+// {
+// 	char	*s1;
+// 	char	*s2;
 
-	s1 = "TESTEEEEEEEA";
-	s2 = "TESTEEEEEEEE";
-	printf("%d", ft_strncmp(s1, s2, 20));
-	return (0);
-}
- */
+// 	s1 = "TESTEEEEEEEA";
+// 	s2 = "TESTEEEEEEEE";
+// 	printf("%d", ft_strncmp(s1, s2, 20));
+// 	return (0);
+// }
