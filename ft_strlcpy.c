331e8fa7c812502f18eb63eed8be4cd13965d389
unknown <victormesini007@gmail.com>
2025-10-21 14:39:12 +0100

    corrections strncmp and strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -10,21 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <stdio.h>
+
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
 	i = 0;
-	if (src[i] == '\0' || size == 0)
-		return (0);
-	while (src[i] != '\0' && i < size)
+	if (i < size)
 	{
-		dst[i] = src[i];
-		i++;
+		while (src[i] != '\0' && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	dst[i - 1] = '\0';
 	while (src[i] != '\0')
 	{
 		i++;
