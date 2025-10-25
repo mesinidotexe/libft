4f68bd24f4e3f269bab1f29136433c9ca113f76c
unknown <victormesini007@gmail.com>
2025-10-25 17:11:13 +0100

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -13,3 +13,42 @@
 #include "libft.h"
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    char *sub;
+    int i;
+
+    i = 0;
+    sub = malloc (sizeof(char) * len);
+    if (!sub)
+        return (NULL);
+    while (*s != '\0' && i < len)
+    {
+        sub[i] = s[start];
+        i++;
+        start++;
+    }
+    sub[i] = '\0';
+    return (sub);
+}
+
+int main(void)
+{
+    char str[] = "Olá, mundo!";
+    char *sub;
+
+    // Exemplo: pegar substring a partir da posição 4, com 5 caracteres
+    sub = ft_substr(str, 4, 5);
+
+    if (sub == NULL)
+    {
+        printf("Erro ao alocar memória.\n");
+        return (1);
+    }
+
+    printf("Substring: %s\n", sub);
+
+    // Libera a memória alocada pela função
+    free(sub);
+
+    return (0);
+}
--- libft.h
+++ libft.h
@@ -26,6 +26,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strdup(const char *s);
+char    *ft_substr(char const *s, unsigned int start, size_t len);
 
 // memory
 void	ft_bzero(void *s, size_t n);
