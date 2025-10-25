b3a16facf93bebd74c655233e634abb674d6c917
unknown <victormesini007@gmail.com>
2025-10-26 00:47:45 +0100

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -20,6 +20,7 @@ char *ft_strjoin(char const *s1, char const *s2)
     {
         newstr[i] = s2[j];
         i++;
+        j++;
     }
     newstr[i + 1] = '\0';
     return (newstr);
@@ -30,7 +31,7 @@ char *ft_strjoin(char const *s1, char const *s2)
     char const *s1 = "Abcd";
     char const *s2 = "Efgh";
     int i = 0;
-    printf("%s\0", ft_strjoin(s1, s2));
+    printf("%s\n", ft_strjoin(s1, s2));
     char *str = ft_strjoin(s1, s2);
     while (str[i])
     {
