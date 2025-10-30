428c3668d839d1612f23a3a35a78bdb79f7ef63e
Victor Mesini Costa <vmesini-@c2r8s3.42lisboa.com>
2025-10-30 00:33:16 +0000

    auto

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,27 +10,50 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <libbsd.h>
 
 char	*ft_strnstr(char *big, char *little, size_t len)
 {
 	size_t	i;
 	size_t	j;
+	size_t	temp;
 
 	i = 0;
 	if (little[i] == '\0')
-		return (big);
-	while (big[i] != '\0')
+		return (&big[i]);
+	while (big[i])
 	{
 		j = 0;
-		while ((little[j] == big[i]) && (little[j + 1] == big[i]) && (j < len))
+		temp = i;
+		while ((little[j] == big[i]) && (j < len))
 		{
+			i++;
 			j++;
 		}
-		if (little[j] == big[i])
+		if ((little[j] == big[i]) && (j <= len))
 		{
-			return (&little[j]);
+			return (&big[temp]);
 		}
 		i++;
 	}
-	return (big);
+	return (NULL);
+}
+
+int	main(void)
+{
+	char big[] = "Eu estou procurando uma palavra!";
+	char little[] = "estou blabla";
+	char *result;
+
+	result = ft_strnstr(big, little, 6);
+
+	if (result)
+	{
+		// result[2] = 0;
+		printf("Encontrado: %s\n", result);
+	}
+	else
+		printf("Não encontrado!\n");
+
+	return (0);
 }
