4206de286dea8c1198dbb81505f2e97cfc8bf11a
Victor Mesini Costa <vmesini-@c2r8s11.42lisboa.com>
2025-11-08 18:43:51 +0000

    auto

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -18,6 +18,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 	int		end;
 	int		i;
 
+	if (!s1 || !set)
+		return (NULL);
 	i = 0;
 	start = 0;
 	end = ft_strlen(s1) - 1;
@@ -25,7 +27,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 		start++;
 	while (end >= 0 && ft_strchr(set, s1[end]))
 		end--;
-	trimmed = malloc(sizeof(char) * end);
+	trimmed = malloc((sizeof(char) * end) + 2);
 	if (!trimmed)
 		return (NULL);
 	while (start <= end)
@@ -38,111 +40,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 /* int	main(void)
 {
-	const char *str1 = "abcabcabcabcab a cabcabcabcabc";
-	const char *str2 = "abc";
+	const char *str1 = "lorem \n ipsum \t dolor \n sit \t amet";
+	const char *str2 = "tel";
 	char *trimmed = ft_strtrim(str1, str2);
 	printf("%s \n", trimmed);
 	free (trimmed);
 	return (0);
 } */
-
-/*char *ft_strtrim(char const *s1, char const *set)
-{
-	char *temp;
-	char *final;
-	int i;
-	int j;
-
-	j = 0;
-	i = 0;
-
-	// se nao tiver set retorn o cast da s1
-	if (set[i] == '\0')
-	{
-		return (temp = (char *)s1);
-	}
-
-	// alocando memoria
-	temp = malloc(sizeof(char) * ft_strlen(s1));
-	if (!temp)
-	{
-		return (NULL);
-	}
-
-	// limpando o comeco da string
-	while (s1[i])
-	{
-		while (s1[i] == set[i])
-		{
-			i++;
-		}
-		temp[j] = s1[i];
-		i++;
-		j++;
-	}
-	temp[j] = '\0';
-	i = ft_strlen(set);
-	// decrementar temp passando sem a ocorrencia final pra trimmed_final
-	while (j > 0)
-	{
-		while (temp[j] == set[i])
-			i--;
-		final[j] = temp[j];
-		j--;
-	}
-	final[ft_strlen(final)] = '\0';
-	return (final);
-}*/
-
-/* char *ft_strtrim(char const *s1, char const *set)
-{
-	char *temp;
-	char *trimmed;
-	int lens;
-	int i;
-	int lenset;
-
-	lens = ft_strlen(s1) - 1;
-	lenset = ft_strlen(set) - 1;
-
-	// se nao tiver set, return o cast da s1
-	if (set[0] == '\0')
-	{
-		return (temp = (char *)s1);
-	}
-
-	// alocando memoria
-	temp = malloc(sizeof(char) * ft_strlen(s1));
-	trimmed = malloc(sizeof(char) * ft_strlen(s1));
-	if (!temp && !trimmed)
-	{
-		return (NULL);
-	}
-
-	//limpando o fim da string
-	while (lens >= 0)
-	{
-		while (s1[lens] == set[lenset])
-		{
-			lens--;
-			lenset--;
-		}
-		temp[lens] = s1[lens];
-		lens--;
-	}
-	lens = 0;
-	i = 0;
-	// limpando o comeco da string
-	while (temp[lens])
-	{
-		while (temp[lens] == set[lens])
-		{
-			lens++;
-		}
-		trimmed[i] = temp[lens];
-		lens++;
-		i++;
-	}
-	trimmed[i] = '\0';
-	return (trimmed);
-}*/
